ba5a091365b2529bb605264fcd19d806b55cefcb
sunnyleeyun <sunnylee945@hotmail.com>
2020-01-20 23:42:28 +0800

    Add challenge 17

---
+++ challenge17/challenge17.playground/Contents.swift
@@ -0,0 +1,10 @@
+import Foundation
+
+func challenge17(min: Int, max: Int) -> Int {
+  return Int.random(in: min...max)
+}
+
+challenge17(min: 1, max: 5)
+challenge17(min: 8, max: 10)
+challenge17(min: 12, max: 12)
+challenge17(min: 12, max: 18)
