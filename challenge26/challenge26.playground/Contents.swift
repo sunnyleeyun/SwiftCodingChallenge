50ad0827bbe467e00c32a9c29e3026153fa534c2
sunnyleeyun <sunnylee945@hotmail.com>
2020-02-09 17:53:30 +0800

    Add challenge 26

---
+++ challenge26/challenge26.playground/Contents.swift
@@ -0,0 +1,8 @@
+import Foundation
+
+func challenge26(subtract: Int, from: Int) -> Int {
+  return from + (-1) * subtract
+}
+
+challenge26(subtract: 5, from: 9)
+challenge26(subtract: 10, from: 30)
