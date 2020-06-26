0c3b33bb176d44cdda23946e0ac7fc4d41cb6c24
sunnyleeyun <sunnylee945@hotmail.com>
2020-06-26 23:00:42 +0800

    Add challenges and challenge 27

--- Challenges/Challenges/main.swift
+++ Challenges/Challenges/main.swift
@@ -2,9 +2,22 @@
 //  main.swift
 //  Challenges
 //
-//  Created by 李昀 on 2020/3/29.
+//  Created by 李昀 on 2020/6/26.
 //  Copyright © 2020 李昀. All rights reserved.
 //
 
-
 import Foundation
+
+func challenge27(fileName: String, lineCount: Int) {
+  guard let text = try? String(contentsOfFile: fileName) else {
+    print("file can't be read.")
+    return
+  }
+  var lines = text.components(separatedBy: "\n")
+  lines.reverse()
+  for i in 0..<min(lines.count, lineCount) {
+    print(lines[i])
+  }
+}
+
+challenge27(fileName: "sampleInput.txt", lineCount: 5)
