3d72db86459f70cc52c64ac6e75ca80444109970
sunnyleeyun <sunnylee945@hotmail.com>
2020-07-05 00:55:10 +0800

    Add challenge 44 and combine with 43

--- challenge43n44/challenge43n44.playground/Contents.swift
+++ challenge43n44/challenge43n44.playground/Contents.swift
@@ -12,6 +12,16 @@ class LinkedListNode<T> {
 class LinkedList<T> {
   var start: LinkedListNode<T>?
   
+  var midNode: LinkedListNode<T>? {
+    var slow = start
+    var fast = start
+    while fast != nil && fast?.next != nil {
+      slow = slow?.next
+      fast = fast?.next?.next
+    }
+    return slow
+  }
+  
   func printNodes() {
     var current = start
     while let node = current {
@@ -21,10 +31,11 @@ class LinkedList<T> {
   }
 }
 
+// challenge 43
 var list = LinkedList<String>()
 var previousNode: LinkedListNode<String>? = nil
 
-for letter in "abcdefghijklmnopqrstuvwxyz" {
+for letter in "abcdefghijklmnopqrstuvwxyz" { // "1, 2, 3, 4, 5"
   let node = LinkedListNode(value: String(letter))
   if let p = previousNode {
     p.next = node
@@ -34,4 +45,6 @@ for letter in "abcdefghijklmnopqrstuvwxyz" {
   previousNode = node
 }
 
-list.printNodes()
+// list.printNodes()
+
+list.midNode
